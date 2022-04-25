f3a5856ed4bd7ab53789357e9c25bd329e9dafb5
Gustavo N <gustavostnt@gmail.com>
2022-04-24 21:41:57 -0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+import "virtual:windi.css";
+import "virtual:windi-devtools";
+import "../css/main.css";
+
+console.log("hello");
+
+// const app = document.querySelector("#app");
+// const div = document.createElement("div");
+// div.innerHTML = `<div class="h-10 w-10 bg-blue-100"></div>`;
+
+// app.appendChild(div);
---
+++ windicss.config.js
@@ -0,0 +1,13 @@
+export default {
+  extract: {
+    include: ["*.{html,js}"],
+    exclude: ["node_modules", ".git"],
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        display: "BorisBlackBloxx",
+      },
+    },
+  },
+};
